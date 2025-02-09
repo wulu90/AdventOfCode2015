74663b478cc2d3c1562c2ccd7b903de4f6fde0dd
wulu <wulu90@outlook.com>
2025-02-09 14:35:20 +0800

    refactoring to a function
    Signed-off-by: wulu <wulu90@outlook.com>

--- 07.cpp
+++ 07.cpp
@@ -83,32 +83,7 @@ instruction parse(const string& str) {
     return inst;
 }
 
-void part1() {
-    ifstream input("input/input07");
-    vector<instruction> ins_vec;
-    for (string line; getline(input, line);) {
-        ins_vec.push_back(parse(line));
-    }
-
-    set<string> unknown;
-    map<string, int> name_val;
-
-    for (auto& ins : ins_vec) {
-        if (ins.input_0.index() == 0) {
-            unknown.insert(get<0>(ins.input_0));
-        }
-        if (ins.g != gate::DIRECT && ins.g != gate::NOT && ins.input_1.index() == 0) {
-            unknown.insert(get<0>(ins.input_1));
-        }
-
-        unknown.insert(ins.output);
-
-        if (ins.g == gate::DIRECT && ins.input_0.index() == 1) {
-            name_val.insert({ins.output, get<1>(ins.input_0)});
-            unknown.erase(ins.output);
-        }
-    }
-
+int run(const vector<instruction>& ins_vec, map<string, int> name_val, set<string> unknown) {
     while (!unknown.empty()) {
         for (auto& ins : ins_vec) {
             if (ins.g == gate::DIRECT && (ins.input_0.index() == 0 && name_val.contains(get<0>(ins.input_0)))) {
@@ -159,8 +134,36 @@ void part1() {
             }
         }
     }
+    return name_val["a"];
+}
 
-    println("{}", name_val["a"]);
+void part1() {
+    ifstream input("input/input07");
+    vector<instruction> ins_vec;
+    for (string line; getline(input, line);) {
+        ins_vec.push_back(parse(line));
+    }
+
+    set<string> unknown;
+    map<string, int> name_val;
+
+    for (auto& ins : ins_vec) {
+        if (ins.input_0.index() == 0) {
+            unknown.insert(get<0>(ins.input_0));
+        }
+        if (ins.g != gate::DIRECT && ins.g != gate::NOT && ins.input_1.index() == 0) {
+            unknown.insert(get<0>(ins.input_1));
+        }
+
+        unknown.insert(ins.output);
+
+        if (ins.g == gate::DIRECT && ins.input_0.index() == 1) {
+            name_val.insert({ins.output, get<1>(ins.input_0)});
+            unknown.erase(ins.output);
+        }
+    }
+
+    println("{}", run(ins_vec, name_val, unknown));
 }
 
 void part2() {
@@ -188,59 +191,9 @@ void part2() {
             unknown.erase(ins.output);
         }
     }
-    name_val["b"] = 46065;
-    while (!unknown.empty()) {
-        for (auto& ins : ins_vec) {
-            if (ins.g == gate::DIRECT && (ins.input_0.index() == 0 && name_val.contains(get<0>(ins.input_0)))) {
-                name_val.insert({ins.output, name_val[get<0>(ins.input_0)]});
-                unknown.erase(ins.output);
-            }
+    name_val["b"] = run(ins_vec, name_val, unknown);
 
-            if (ins.g == gate::NOT && name_val.contains(get<0>(ins.input_0))) {
-                name_val.insert({ins.output, ~name_val[get<0>(ins.input_0)]});
-                unknown.erase(ins.output);
-            }
-
-            if (ins.g == gate::AND || ins.g == gate::OR) {
-                bool b0 = false;
-                bool b1 = false;
-                int n0, n1;
-                if (ins.input_0.index() == 0 && name_val.contains(get<0>(ins.input_0))) {
-                    b0 = true;
-                    n0 = name_val[get<0>(ins.input_0)];
-                }
-                if (ins.input_0.index() == 1) {
-                    b0 = true;
-                    n0 = get<1>(ins.input_0);
-                }
-
-                if (ins.input_1.index() == 0 && name_val.contains(get<0>(ins.input_1))) {
-                    b1 = true;
-                    n1 = name_val[get<0>(ins.input_1)];
-                }
-                if (ins.input_1.index() == 1) {
-                    b1 = true;
-                    n1 = get<1>(ins.input_0);
-                }
-
-                if (b0 && b1) {
-                    name_val[ins.output] = ins.g == gate::AND ? n0 & n1 : n0 | n1;
-                    unknown.erase(ins.output);
-                }
-            }
-
-            if (ins.g == gate::LSHIFT || ins.g == gate::RSHIFT) {
-                if (name_val.contains(get<0>(ins.input_0))) {
-                    int a                = name_val[get<0>(ins.input_0)];
-                    int b                = get<1>(ins.input_1);
-                    name_val[ins.output] = ins.g == gate::LSHIFT ? a << b : a >> b;
-                    unknown.erase(ins.output);
-                }
-            }
-        }
-    }
-
-    println("{}", name_val["a"]);
+    println("{}", run(ins_vec, name_val, unknown));
 }
 
 int main() {
