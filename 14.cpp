8c03178d656e732292963caed16dbaec2d37a2f8
wulu <wulu90@outlook.com>
2025-02-10 20:28:16 +0800

    14-2
    Signed-off-by: wulu <wulu90@outlook.com>

--- 14.cpp
+++ 14.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <map>
 #include <print>
 #include <ranges>
 #include <string>
@@ -50,7 +51,38 @@ void part1() {
     println("{}", *max_element(dist_vec.begin(), dist_vec.end()));
 }
 
+void part2() {
+    ifstream input("input/input14");
+    vector<reindeer> deer_vec;
+    for (string line; getline(input, line);) {
+        deer_vec.push_back(parse(line));
+    }
+
+    const static int time = 2503;
+
+    vector<int> deer_points(deer_vec.size(), 0);
+    for (int t = 1; t <= time; ++t) {
+        multimap<int, size_t> dist_inx;
+        for (size_t i = 0; i < deer_vec.size(); ++i) {
+            auto& rd    = deer_vec[i];
+            int fly_num = t / (rd.fly + rd.rest);
+            int m_t     = t % (rd.fly + rd.rest);
+            m_t         = m_t >= rd.fly ? rd.fly : m_t;
+            dist_inx.insert({fly_num * rd.speed * rd.fly + rd.speed * m_t, i});
+        }
+
+        auto max_dist = dist_inx.rbegin()->first;
+        auto [b, e]   = dist_inx.equal_range(max_dist);
+        for (auto it = b; it != e; ++it) {
+            ++deer_points[it->second];
+        }
+    }
+
+    println("{}", *max_element(deer_points.begin(), deer_points.end()));
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
