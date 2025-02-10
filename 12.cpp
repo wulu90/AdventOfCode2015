2de578b71fcc2e84d7b29649e71ef686db67ec2c
wulu <wulu90@outlook.com>
2025-02-10 16:16:27 +0800

    12-2
    Signed-off-by: wulu <wulu90@outlook.com>

--- 12.cpp
+++ 12.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <print>
 
+#include <boost/json.hpp>
+
 using namespace std;
 
 void part1() {
@@ -30,7 +32,43 @@ void part1() {
     println("{}", sum);
 }
 
+int sum_except_red(const boost::json::value& val) {
+    int sum = 0;
+    if (val.is_int64()) {
+        sum += val.as_int64();
+    } else if (val.is_array()) {
+        for (auto& arr : val.as_array()) {
+            sum += sum_except_red(arr);
+        }
+    } else if (val.is_object()) {
+        bool red_property = false;
+        for (auto& [k, v] : val.as_object()) {
+            if (v.is_string() && v.as_string() == "red"s) {
+                red_property = true;
+                break;
+            }
+        }
+
+        if (red_property) {
+            return 0;
+        } else {
+            for (auto& [k, v] : val.as_object()) {
+                sum += sum_except_red(v);
+            }
+        }
+    }
+    return sum;
+}
+
+void part2() {
+    ifstream input("input/input12");
+
+    boost::json::value val = boost::json::parse(input);
+    println("{}", sum_except_red(val));
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
