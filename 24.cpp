16d9a6ce2a958193896e27081a98c353ec0cad29
wulu <wulu90@outlook.com>
2025-02-12 01:23:42 +0800

    24-2
    Signed-off-by: wulu <wulu90@outlook.com>

--- 24.cpp
+++ 24.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <fstream>
+#include <map>
 #include <print>
 #include <vector>
 
@@ -42,7 +43,40 @@ void part1() {
     println("{}", qe);
 }
 
+void part2() {
+    ifstream input("input/input24");
+    vector<int> vec;
+    int sum = 0;
+    for (int i; input >> i;) {
+        vec.push_back(i);
+        sum += i;
+    }
+
+    vector<vector<size_t>> combinatio_vec;
+    combination_sum(0, 0, {}, combinatio_vec, sum / 4, vec);
+
+    multimap<size_t, size_t> size_count;
+    for (size_t i = 0; i < combinatio_vec.size(); ++i) {
+        size_count.insert({combinatio_vec[i].size(), i});
+    }
+
+    auto lowest_container_count = size_count.begin()->first;
+    auto [b, e]                 = size_count.equal_range(lowest_container_count);
+
+    vector<int64_t> qe_vec;
+    for (auto it = b; it != e; ++it) {
+        int64_t qe = 1;
+        for (auto& a : combinatio_vec[it->second]) {
+            qe *= vec[a];
+        }
+        qe_vec.push_back(qe);
+    }
+
+    println("{}", ranges::min(qe_vec));
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
